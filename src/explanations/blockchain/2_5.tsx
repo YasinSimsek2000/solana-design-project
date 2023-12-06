0274bca88371ab71c6be0e21f60e26259b34f561
Serkan <serkankaragoz2002@gmail.com>
2023-12-06 14:45:47 +0300

    Changed theme from dark to light and added br between components

--- src/explanations/blockchain/2_5.tsx
+++ src/explanations/blockchain/2_5.tsx
@@ -11,7 +11,7 @@ export const Data2_5 = () => {
             <p>
                 As we’ve seen, most of these alternative protocols employ some type of pay-to-play scheme. Proof of capacity is no different, but here you &apos;pay&apos; with hard drive space. The more hard drive space you have, the better your chance of mining the next block and earning the block reward.
             </p>
-            
+            <br/>
             <p>
                 Prior to mining in a proof-of-capacity system, the algorithm generates large data sets known as &apos;plots&apos;, which you store on your hard drive. The more plots you have, the better your chance of finding the next block in the chain.
             </p>
